16bdcbf50f20c503bc7b5d12349615abce205b2a
Mirlan Ipasov <mipasov@gmail.com>
2020-09-18 00:04:16 +0600

    main added, prints greetings!

---
+++ src/com/coolstartup/Main.java
@@ -0,0 +1,9 @@
+package com.coolstartup;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello , I am cool project!");
+    }
+}
